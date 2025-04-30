f47710be1a47b90ec212f85591428dfa8ed8f4fe
GonzaGDG <contacto.gge@gmail.com>
2025-04-30 15:59:29 -0400

    Update nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -117,7 +117,7 @@ function desktopParallax() {
 
   window.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
-    heroImage.style.transform = `translateY(${scrollY * 0.3}px)`;
+    heroImage.style.transform = `translateY(${scrollY * 0.5}px)`;
   });
 }
 
